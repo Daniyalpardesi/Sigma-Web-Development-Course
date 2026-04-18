6a7ff8a02fa7fc039ee84dedf9973fd502b1b21b
Daniyalpardesi <163700913+Daniyalpardesi@users.noreply.github.com>
2026-04-18 23:39:39 +0000

    .

---
+++ Projects/Back to Basic Web Development Course (BTBWD)/Projects/Script.js
@@ -0,0 +1,7 @@
+const input = Document.getElementbyid("Todo-Input")
+const addBtn = Document.getElementbyid("Add-Btn")
+const list = Document.getElementbyid("todo-List")
+
+function SaveTodos(){
+    
+}
